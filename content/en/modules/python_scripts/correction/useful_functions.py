d294f4cde66ba66ffde20886c8e07210d71875f0
FrancoisPgm <francois.paugam@laposte.net>
2021-10-29 13:24:44 -0400

    replace python scripts exercice with the bonus one

--- content/en/modules/python_scripts/correction/useful_functions.py
+++ content/en/modules/python_scripts/correction/useful_functions.py
@@ -1,16 +1,20 @@
-def change_case(string):
-    if len(string) > 0:
-        new_string = string[:-1].lower() + string[-1].upper()
-    else:
-        new_string = ""
-    return new_string
+def encrypt_letter(msg, key):
+    enc_id = (ord(msg) + ord(key)) % 1114112
+    return chr(enc_id)
 
-def get_words(string):
-    return string.split(" ")
 
-def join_words(string_list):
-    new_string = ""
-    for word in string_list:
-        new_string += word + " "
-    new_string = new_string[:-1] # remove last space
-    return new_string
+def decrypt_letter(msg, key):
+    dec_id = (1114112 + ord(msg) - ord(key)) % 1114112
+    return chr(dec_id)
+
+
+def process_message(message, key, encrypt):
+    returned_message = ""
+
+    for i, letter in enumerate(message):
+        if encrypt:
+            returned_message += encrypt_letter(letter, key[i%len(key)])
+        else:
+            returned_message += decrypt_letter(letter, key[i%len(key)])
+
+    return returned_message
